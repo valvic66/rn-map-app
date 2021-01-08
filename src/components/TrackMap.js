c4dc0eaacec93ec786a65ce5db34d5fcc93342ae
valvic6 <valvic6@192-168-0-100.rdsnet.ro>
2021-01-08 16:22:17 +0200

    added a map with present location for testing purposes

--- src/components/TrackMap.js
+++ src/components/TrackMap.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import { Text } from 'react-native-elements';
 import { StyleSheet } from 'react-native';
+import MapView, { Polyline } from 'react-native-maps';
 
 const TrackMap = () => {
+  const points = [
+    { latitude: 44.1905, longitude: 28.6310 },
+    { latitude: 44.2905, longitude: 28.7310 },
+  ];
+  
   return (
     <>
       <Text>Map location here</Text>
+      <MapView 
+        style={styles.mapStyle}
+        initialRegion={{
+          latitude: 44.1905,
+          longitude: 28.6310,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01
+        }}
+      >
+        <Polyline coordinates={points} />
+      </MapView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-
+  mapStyle: {
+    height: 350
+  }
 });
 
 export default TrackMap;
